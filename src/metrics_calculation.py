cc225675f6c9e9b6b291514e361921310b7a8444
Farah <farah.assgari@gmail.com>
2024-08-13 19:58:22 -0400

    updated metrics

--- src/metrics_calculation.py
+++ src/metrics_calculation.py
@@ -40,6 +40,45 @@ def calculate_metrics(model_pred_df, genre_list, genre_true_counts, genre_tp_cou
 
     # Your code here
 
+    tc_g = 0
+    tp_g = 0
+    fp_g = 0
+    fn_g = 0
+    
+    for g in genre_list:
+        tc_g += genre_true_counts.get(g,0)
+        tp_g += genre_tp_counts.get(g,0)
+        fp_g += genre_fp_counts.get(g,0)
+        fn_g += (genre_true_counts.get(g,0) - genre_tp_counts.get(g,0))
+
+    micro_prec = tp_g/(tp_g+fp_g)
+    micro_rec = tp_g/(tp_g+fn_g)
+    micro_f1 = 2 * ((micro_prec*micro_rec)/(micro_prec+micro_rec))
+
+    #print(micro_prec)
+    #print(micro_rec)    
+
+    macro_prec_list = []
+    macro_rec_list = []
+    macro_f1_list = []
+
+    for g in genre_list:
+        tc_g_mac = genre_true_counts.get(g,0)
+        tp_g_mac = genre_tp_counts.get(g,0)
+        fp_g_mac = genre_fp_counts.get(g,0)
+        fn_g_mac = (genre_true_counts.get(g,0) - genre_tp_counts.get(g,0)) 
+
+        macro_prec = tp_g_mac/(tp_g_mac+fp_g_mac)
+        macro_prec_list.append(macro_prec)
+        macro_rec = tp_g_mac/(tp_g_mac+fn_g_mac)
+        macro_rec_list.append(macro_rec)
+        macro_f1 = 2 * ((macro_prec*macro_rec)/(macro_prec+macro_rec))
+        macro_f1_list.append(macro_f1)
+
+
+    return micro_prec, micro_rec, micro_f1, macro_prec_list, macro_rec_list, macro_f1_list
+
+
     
 def calculate_sklearn_metrics(model_pred_df, genre_list):
     '''
